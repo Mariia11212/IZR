b38d361543e999b68739126d335c3c35d87e8dfd
Mariia11212 <yanytskamaria@gmail.com>
2025-11-01 16:14:01 +0200

    unut-tests for модуль MyDynamicArrayBasedList<T> 14шт

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,10 +1,235 @@
-﻿using System;
+﻿using lr1;
+using lr1.DataStructures;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace lr1.Tests
 {
+    [TestFixture]
+    public class MyDataTests
+    {
+        // МОДУЛЬ: MyDynamicArrayBasedList<T>
+        [TestFixture]
+        public class MyDynamicArrayBasedListTests
+        {
+            // Тест #1: Перевіряємо, що щойно створений список є порожнім і має Count = 0.
+            [Test]
+            public void NewList_ShouldBeEmptyAndHaveZeroCount()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                Assert.That(list.Count, Is.EqualTo(0));
+            }
 
+            // Тест #2: Перевіряємо, що метод Add коректно збільшує лічильник.
+            [Test]
+            public void Add_ShouldIncrementCount()
+            {
+                var list = new MyDynamicArrayBasedList<string>();
+                list.Add("hello");
+                list.Add("world");
+                Assert.That(list.Count, Is.EqualTo(2));
+            }
+
+            // Тест #3: Перевіряємо, що список автоматично розширюється, коли кількість елементів перевищує початкову ємність (4).
+            [Test]
+            public void Add_BeyondInitialCapacity_ShouldResizeAndAddSuccessfully()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                // Додаємо 5 елементів, щоб перевищити початкову ємність 4
+                for (int i = 1; i <= 5; i++)
+                {
+                    list.Add(i * 10);
+                }
+                Assert.That(list.Count, Is.EqualTo(5));
+                Assert.That(list.GetAt(4), Is.EqualTo(50)); // Перевіряємо останній доданий елемент
+            }
+
+            // Тест #4: Перевіряємо, що GetAt повертає правильний елемент за валідним індексом.
+            [Test]
+            public void GetAt_ValidIndex_ShouldReturnCorrectElement()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                list.Add(20);
+                Assert.That(list.GetAt(1), Is.EqualTo(20));
+            }
+
+            // Тест #5: Перевіряємо, що GetAt кидає виняток для невалідного (від'ємного) індексу.
+            [Test]
+            public void GetAt_NegativeIndex_ShouldThrowArgumentOutOfRangeException()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                Assert.Throws<ArgumentOutOfRangeException>(() => list.GetAt(-1));
+            }
+
+            // Тест #6: Перевіряємо, що GetAt кидає виняток для індексу, що дорівнює або перевищує Count.
+            [Test]
+            public void GetAt_IndexEqualToCount_ShouldThrowArgumentOutOfRangeException()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                Assert.Throws<ArgumentOutOfRangeException>(() => list.GetAt(1));
+            }
+
+            // Тест #7: Перевіряємо, що InsertAt вставляє елемент у середину списку, зміщуючи інші елементи.
+            [Test]
+            public void InsertAt_MiddleOfList_ShouldShiftElementsAndIncrementCount()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                list.Add(30);
+                list.InsertAt(20, 1); // Вставляємо 20 в індекс 1
+                Assert.That(list.Count, Is.EqualTo(3));
+                Assert.That(list.GetAt(0), Is.EqualTo(10));
+                Assert.That(list.GetAt(1), Is.EqualTo(20));
+                Assert.That(list.GetAt(2), Is.EqualTo(30));
+            }
+
+            // Тест #8: Перевіряємо, що RemoveAt видаляє елемент і коректно зміщує решту елементів.
+            [Test]
+            public void RemoveAt_ShouldRemoveElementAndDecrementCount()
+            {
+                var list = new MyDynamicArrayBasedList<string>();
+                list.Add("A");
+                list.Add("B");
+                list.Add("C");
+                list.RemoveAt(1); // Видаляємо "B"
+                Assert.That(list.Count, Is.EqualTo(2));
+                Assert.That(list.GetAt(1), Is.EqualTo("C"));
+            }
+
+            // Тест #9: Перевіряємо, що IndexOf знаходить індекс першого входження елемента.
+            [Test]
+            public void IndexOf_ExistingItem_ShouldReturnCorrectIndex()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                list.Add(20);
+                list.Add(10);
+                Assert.That(list.IndexOf(10), Is.EqualTo(0));
+            }
+
+            // Тест #10: Перевіряємо, що IndexOf повертає -1, якщо елемента немає у списку.
+            [Test]
+            public void IndexOf_NonExistingItem_ShouldReturnMinusOne()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                Assert.That(list.IndexOf(99), Is.EqualTo(-1));
+            }
+
+
+            // Тест #11: Перевіряємо, що FindFirst знаходить правильний елемент за предикатом.
+            [Test]
+            public void FindFirst_WithMatchingPredicate_ShouldReturnItem()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(5);
+                list.Add(15);
+                list.Add(25);
+                var result = list.FindFirst(x => x > 10);
+                Assert.That(result, Is.EqualTo(15));
+            }
+
+            // Тест #12: Перевіряємо, що FindFirst повертає default, якщо жоден елемент не відповідає предикату.
+            [Test]
+            public void FindFirst_WithNoMatchingPredicate_ShouldReturnDefault()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(5);
+                var result = list.FindFirst(x => x > 100);
+                Assert.That(result, Is.EqualTo(default(int))); // default(int) is 0
+            }
+
+            // Тест #13: Перевіряємо, що метод Clear робить список порожнім (Count = 0).
+            [Test]
+            public void Clear_ShouldResetCountToZero()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(1);
+                list.Add(2);
+                list.Clear();
+                Assert.That(list.Count, Is.EqualTo(0));
+            }
+
+            // Тест #14: Перевіряємо, що енумератор дозволяє коректно ітерувати по списку за допомогою foreach.
+            [Test]
+            public void GetEnumerator_ShouldAllowIterationThroughAllItems()
+            {
+                var list = new MyDynamicArrayBasedList<int>();
+                list.Add(10);
+                list.Add(20);
+                list.Add(30);
+
+                var iteratedItems = new List<int>();
+                foreach (var item in list)
+                {
+                    iteratedItems.Add(item);
+                }
+
+                CollectionAssert.AreEqual(new[] { 10, 20, 30 }, iteratedItems);
+            }
+        }
+    } 
+    // МОДУЛЬ: Vector
+
+    [TestFixture]
+    public class VectorTests
+    {
+        // Тест #1: Перевіряємо, що конструктор правильно ініціалізує властивості Dx та Dy.
+        [Test]
+        public void Constructor_ShouldSetDxAndDyProperties()
+        {
+            var v = new Vector(1.5, -2.5);
+            Assert.That(v.Dx, Is.EqualTo(1.5));
+            Assert.That(v.Dy, Is.EqualTo(-2.5));
+        }
+
+        // Тест #2: Перевіряємо правильність обчислення довжини (магнитуди) вектора.
+        [Test]
+        public void GetMagnitude_ForPythagoreanTriple_ShouldCalculateCorrectLength()
+        {
+            var v = new Vector(3, 4);
+            // Викликаємо метод GetMagnitude()
+            double magnitude = v.GetMagnitude();
+            // Використовуємо Within для порівняння чисел з плаваючою комою
+            Assert.That(magnitude, Is.EqualTo(5).Within(0.001));
+        }
+
+        // Тест #3: Перевіряємо, що GetMagnitude для нульового вектора повертає 0.
+        [Test]
+        public void GetMagnitude_ForZeroVector_ShouldBeZero()
+        {
+            var v = new Vector(0, 0);
+            Assert.That(v.GetMagnitude(), Is.EqualTo(0));
+        }
+
+        // Тест #4: Перевіряємо, що метод Scale повертає новий вектор з правильними, масштабованими значеннями.
+        [Test]
+        public void Scale_ShouldReturnNewScaledVector()
+        {
+            var v = new Vector(2, 3);
+            // Викликаємо метод Scale()
+            var scaledVector = v.Scale(3);
+            Assert.That(scaledVector.Dx, Is.EqualTo(6));
+            Assert.That(scaledVector.Dy, Is.EqualTo(9));
+        }
+
+        // Тест #5: Перевіряємо, що метод Scale не змінює оригінальний вектор (перевірка на іммутабельність).
+        [Test]
+        public void Scale_ShouldNotModifyOriginalVector()
+        {
+            var originalVector = new Vector(5, 10);
+            originalVector.Scale(2); // Викликаємо метод, але не зберігаємо результат
+
+            // Переконуємось, що оригінальний вектор залишився незмінним
+            Assert.That(originalVector.Dx, Is.EqualTo(5));
+            Assert.That(originalVector.Dy, Is.EqualTo(10));
+        }
+    }
 }
--- obj/Debug/net9.0/lr1.AssemblyInfo.cs
+++ obj/Debug/net9.0/lr1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lr1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0424ad728472bcfc78505fdbfc90617e1cd0b6e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a896c589d82b1f7f8e4ec08d5675e59c3bb4ea3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lr1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lr1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
